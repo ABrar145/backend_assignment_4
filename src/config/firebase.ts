34bc31a69966fca9ae6637f62bbc956cf2e55a0f
Amninder Brar <abrar14@rrc.ca>
2025-03-08 03:22:58 -0600

    feat: added key in firebase.ts file.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getAuth, Auth } from "firebase-admin/auth";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
+import  serviceAccount from "../../assignment04-ba6c3-firebase-adminsdk-fbsvc-b4a4168fe9.json";
  
 // Initialize Firebase Admin SDK
 initializeApp({
