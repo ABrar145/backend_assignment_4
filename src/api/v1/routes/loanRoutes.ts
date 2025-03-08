65c07a41c3d2da8b7f0f72b6c61b36f7de20aa2a
Amninder Brar <abrar14@rrc.ca>
2025-03-08 04:27:31 -0600

    feat: secure admin role assignment route with authorization in loan routes.

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { getLoans, createLoan, updateLoanStatus } from '../controllers/loanController';
 import authenticate from '../middleware/authenticate'; 
+import isAuthorized from "../middleware/authorize";
 
 const router = Router();
 
 // Define the API routes
-router.post("/loans",authenticate, createLoan);
-router.put("/loans/:id/review", authenticate,updateLoanStatus);
-router.get("/loans",authenticate, getLoans);
-router.put("/loans/:id/approve",authenticate, updateLoanStatus);
+router.post("/loans",authenticate,isAuthorized({ hasRole: ["user"] }), createLoan);
+router.put("/loans/:id/review", authenticate,isAuthorized({ hasRole: ["officer"] }),updateLoanStatus);
+router.get("/loans",authenticate,isAuthorized({ hasRole: ["officer", "manager"] }), getLoans);
+router.put("/loans/:id/approve",authenticate,isAuthorized({ hasRole: ["manager"] }), updateLoanStatus);
 
 export default router;
