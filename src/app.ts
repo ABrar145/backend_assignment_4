ecfe6ac21822857cbacc578d508b8ebb1115bd08
Amninder Brar <abrar14@rrc.ca>
2025-03-08 03:00:13 -0600

    feat: added loan routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
+import loanRoutes from "./api/v1/routes/loanRoutes";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(morgan('dev'));
+
+// Routes
+app.use("/api/v1", loanRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
