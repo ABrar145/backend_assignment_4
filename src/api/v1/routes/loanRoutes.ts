d54f065edfd7a9ec59d5cc4caccaf4d29141bf1f
Amninder Brar <abrar14@rrc.ca>
2025-03-08 04:11:09 -0600

    feat: added authenticate to the loan route.

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { getLoans, createLoan, updateLoanStatus } from '../controllers/loanController';
+import authenticate from '../middleware/authenticate'; 
 
 const router = Router();
 
 // Define the API routes
-router.post("/loans", createLoan);
-router.put("/loans/:id/review", updateLoanStatus);
-router.get("/loans", getLoans);
-router.put("/loans/:id/approve", updateLoanStatus);
+router.post("/loans",authenticate, createLoan);
+router.put("/loans/:id/review", authenticate,updateLoanStatus);
+router.get("/loans",authenticate, getLoans);
+router.put("/loans/:id/approve",authenticate, updateLoanStatus);
 
 export default router;
